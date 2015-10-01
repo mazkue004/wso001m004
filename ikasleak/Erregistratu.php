ebae0053622143b60cc8d87915cf1053a608adfc
Maitane Azkue Zaldua <maitane07@gmail.com>
2015-10-01 10:44:45 +0200

    2LabHasi

---
+++ ikasleak/Erregistratu.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html>
+	<head>	
+		<title>Erregistratu</title>
+		<meta name="generator"
+		content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+		<link href="http://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Muli:300,400" rel="stylesheet" type="text/css">
+		<style>
+			body {
+			font: 400 16px 'Muli', sans-serif !important;
+			margin: 0;
+			padding: 0;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			mysql_connect("localhost","root","") or die(mysql_error());
+			mysql_select_db("quiz") or die(mysql_error());
+			
+			$erabepost = mysql_query("select * from erabiltzailea");
+			$row = mysql_fetch_array($erabepost);
+				echo '<p>sdlkfasj</p><br/>';
+				echo '<p>'.$row['Eposta'].'</p>';
+			
+			
+			
+			
+			
+			?>
+	</body>
+</html>
