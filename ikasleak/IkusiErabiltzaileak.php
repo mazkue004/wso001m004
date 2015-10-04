c67ab0f4a32f95fa02aec3f95febd2b99914c021
Maitane Azkue Zaldua <maitane07@gmail.com>
2015-10-04 23:59:46 +0200

    Lab2, IkusiErabiltzaileak.php txukunduta

--- ikasleak/IkusiErabiltzaileak.php
+++ ikasleak/IkusiErabiltzaileak.php
@@ -5,12 +5,13 @@
 	
 	
 	$erabiltzaile = mysql_query("select * from erabiltzaile");
-	echo '<table border=1><tr><th> IZENA </th><th> EPOSTA </th></tr>';
+	echo '<div class="container-fluid inner">
+			<table class="tableizer-table">
+				<tr class="tableizer-firstrow"><th>Izena</th><th>1 Abizena</th><th>2 Abizena</th><th>Eposta</th><th>Telefono zenbakia</th><th>Espezialitatea</th><th>Interesak</th><th>Argazkia</th></tr>';
 	while( $row = mysql_fetch_array( $erabiltzaile) ) {
-		echo '<tr><td>'.$row['Izena'].'</td> <td>'. $row['Eposta'].
-		'</td></tr>';
+		echo '<tr><td>'.$row['Izena'].'</td><td>'. $row['Abizena1'].'</td><td>'.$row['Abizena2'].'</td><td>'.$row['Eposta'].'</td><td>'.$row['Telefonoa'].'</td><td>'.$row['Espezialitatea'].'</td><td>'.$row['Erremintak'].'</td><td>'.$row['Argazkia'].'</td></tr>';
 	}
-	echo '</table>';
+	echo '</table></div>';
 	
 ?>
 
@@ -23,6 +24,7 @@
 		content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
 		<link href="http://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Muli:300,400" rel="stylesheet" type="text/css">
+		<link rel='stylesheet' type='text/css' href='stylesPWS/credits.css' />
 		<style>
 			body {
 			font: 400 16px 'Muli', sans-serif !important;
