03f869cf1250942d8cfa1179605057d3f6981fdb
Maitane Azkue Zaldua <maitane07@gmail.com>
2015-10-15 13:04:47 +0200

    3Labo.1

---
+++ ikasleak/LogIn.php
@@ -0,0 +1,76 @@
+<?php
+	mysql_connect("localhost","root","") or die(mysql_error());
+	mysql_select_db("quiz") or die(mysql_error());
+	
+	//Eposta eta pasahitza emaitza emateko
+	$em = -1;
+	
+	//Eposta konprobatu
+	if(isset($_POST['eposta']) && isset($_POST['pass'])){
+		if(!filter_var($_POST['eposta'], FILTER_VALIDATE_REGEXP,array("options"=>array("regexp"=>"/[a-zA-Z]+[0-9]{3}\@ikasle\.ehu\.eu?s/"))) === false){
+			$erab = mysql_query("select Eposta,Pasahitza from erabiltzaile where Eposta='$_POST[eposta]'");
+			if($erabiltzaile=mysql_fetch_array($erab)){
+				$pasahitza = $erabiltzaile['Pasahitza'];
+				if($_POST['pass'] == $pasahitza){
+					$em=0;
+					} else{
+					$em=1;
+					
+				}
+			}
+			mysql_close();
+		}else{
+			$em = 2;
+		}
+	} 
+	
+	
+	
+	
+?>
+
+<!DOCTYPE html>
+
+<html>
+	<head>	
+		<title>Log in</title>
+		<meta name="generator"
+		content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+		<link href="http://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Muli:300,400" rel="stylesheet" type="text/css">
+		<style>
+			body {
+			font: 400 16px 'Muli', sans-serif !important;
+			margin: 0;
+			padding: 0;
+			}
+			#loginGaizki{
+			color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<br/>
+		<div class="container-fluid inner" >
+			<form id="erregistro" name="erregistro" method="POST" action="LogIn.php">
+				Eposta elektronikoa(*): <input type="text" name="eposta" id="eposta" required placeholder="ehu001@ikasle.ehu.es"/> <br/><br/>
+				Password(*): <input type="password" name="pass" id="pass" required /> <br/><br/>
+				<input type="submit" value="Log in"/>
+			</form>
+			<br/>
+			<?php 
+				if ($em == 0){
+					echo '<label id="loginOndo">Log in ondo egin da</label><br/>';
+					echo '<a  id="home" href="layout.html">Quiz</a>';
+					} else if($em == 1){
+					echo '<label id="loginGaizki">Pasahitza okerra</label>';
+				} 
+				if($em == 2){
+					echo '<label id="loginGaizki">Eposta formatua okerra</label>';
+				}
+			?>
+			<br/><br/>
+			<a  id="home" href='layout.html'>Home</a>
+		</div>
+	</body>
+</html>
